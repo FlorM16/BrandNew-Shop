8a54e77fe9523038258f613ae64bec9b90fe6731
Flor Molina <flormoly9@gmail.com>
2023-11-11 01:13:30 -0500

    Unimos el navbar y el menu mobile

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
     /*Para el elemento HTML que tiene la clase .desktop-menu, pondremos/quitaremos la clase inactive, dependiendo de si la tiene o no.*/
 
+}
+
+function toggleMobileMenu(){
+    console.log("probando");
+    mobileMenu.classList.toggle('inactive');
 }
