797cbab2fc297222cc36ca334bb6e829738b9e07
Flor Molina <flormoly9@gmail.com>
2023-11-11 19:19:58 -0500

    Lista de productos. Creamos HTML a partir de arrays

--- main.js
+++ main.js
@@ -4,11 +4,11 @@ const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
-
 
 function toggleDesktopMenu(){
     const isCarritoAside = aside.classList.contains('inactive');
@@ -46,9 +46,88 @@ function toggleCarritoAside(){
         desktopMenu.classList.add('inactive');
     }
 
-
     aside.classList.toggle('inactive');
-
-   
-    
 }
+
+const productList = [];
+
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
+productList.push({
+    name: 'Scooter',
+    price: 130,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
+productList.push({
+    name: 'Longboard',
+    price: 150,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
+
+/*Crearemos esta estructura HTML con JS, para que cada vez que registremos un producto, se agregue automaticamente desde la base de datos y sea dinamico*/
+/* <div class="product-card">
+    <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" class="product-img">
+    <div class="product-info">
+        <div>
+            <p>$120,00</p>
+            <p>Bike</p>
+        </div>
+        <figure>
+            <img src="icons/bt_add_to_cart.svg" alt="">
+        </figure>
+    </div>
+</div> */
+
+
+
+function renderProducts(arr){
+/*Haremos una iteracion de cada uno de nuestros productos y por cada producto estaremos maquetando una nueva estructura HTML, cada producto tendrá datos distintos que se iran agregando dinamicamente a medida que se cree cada producto.*/
+for (product of arr){
+    // 'product' es cada uno de los objetos que tengamos en nuestro array productList(ahora arr)
+    // of: nos da el elemento del array
+    // in: nos da el indice del elemento del array
+
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+  
+    // product= {name, price, image} -> product.image
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src', product.image);
+  
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+  
+    const productInfoDiv = document.createElement('div');
+  
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$' + product.price;
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+  
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+  
+    const productInfoFigure = document.createElement('figure');
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+  
+    productInfoFigure.appendChild(productImgCart);
+  
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+  
+    productCard.appendChild(productImg);
+    productCard.appendChild(productInfo);
+  
+    cardsContainer.appendChild(productCard);
+
+}
+}
+
+renderProducts(productList);
